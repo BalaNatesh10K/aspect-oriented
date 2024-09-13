56aa29c3c88a23b524620f28eafe0dc56e69871f
Administrator <balanatesh3@gmail.com>
2024-09-13 11:53:20 +0530

    new

--- src/main/java/com/aspectConcepts/aspect_oriented/AspectOrientedApplication.java
+++ src/main/java/com/aspectConcepts/aspect_oriented/AspectOrientedApplication.java
@@ -2,8 +2,14 @@ package com.aspectConcepts.aspect_oriented;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.aspectConcepts.aspect-oriented")
+@PropertySource("classpath:application.properties")
 public class AspectOrientedApplication {
 
 	public static void main(String[] args) {
